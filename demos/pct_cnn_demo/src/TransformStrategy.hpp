159cbaee4da57370b57dae6779b3a272e00d9470
Michael Opitz <michael.w.opitz@gmail.com>
2014-04-17 20:06:55 +0200

    Die PFHTransformStrategy ist jetzt getemplated.

--- demos/pct_cnn_demo/src/TransformStrategy.hpp
+++ demos/pct_cnn_demo/src/TransformStrategy.hpp
@@ -7,11 +7,16 @@
 using namespace pcl;
 using namespace std;
 
+
+template<typename PointT>
 class TransformStrategy
 {
 public:
-    virtual ~TransformStrategy() {}
-    virtual PointCloud<PointXYZRGB>::Ptr transform(const PointCloud<PointXYZRGB>::Ptr source, const PointCloud<PointXYZRGB>::Ptr target) = 0;
+    virtual ~TransformStrategy<PointT>() {}
+    virtual typename PointCloud<PointT>::Ptr transform(
+            const typename PointCloud<PointT>::Ptr source,
+            const typename PointCloud<PointT>::Ptr target) = 0;
+    virtual void loadDefaultSettings() = 0;
 };
 
 #endif
