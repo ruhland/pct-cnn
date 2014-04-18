c5a05efc2961f409a1744a2877ee126721b89c40
Michael Opitz <michael.w.opitz@gmail.com>
2014-04-18 16:40:33 +0200

    Meine Demo.cpp nimmt zwei Input-PointClouds und transformiert die erste theoretisch...

--- demos/pct_cnn_demo/src/Demo.cpp
+++ demos/pct_cnn_demo/src/Demo.cpp
@@ -5,35 +5,54 @@
 
 #include "Demo.hpp"
 
-Demo::Demo(): visualizer(){
-   std::cout<<"Demo created";
-   transformer= new PFHTransformStrategy();
+Demo::Demo(): visualizer()
+{
+   std::cout << "Demo created";
+   transformer = new PFHTransformStrategy<PointXYZRGB>();
 }
 
-void Demo::setInputFile(const std::string& name){
-	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_in 	(new pcl::PointCloud<pcl::PointXYZRGB>);
-	pcl::io::loadPCDFile (name, *cloud_in);
-	visualizer.setInputPC(cloud_in);
-	visualizer.setTransformedPC(transformer->transform(cloud_in, cloud_in));
+void Demo::setSourceCloud(const std::string &filename,
+        PointCloud<PointXYZRGB>::Ptr &source_cloud)
+{
+    io::loadPCDFile (filename, *source_cloud);
+    visualizer.setSourceCloud<PointXYZRGB>(source_cloud);
 }
-void Demo::setOutputFile(const std::string& output){}
-void Demo::enableKinect(){}
-void Demo::run(){
-	visualizer.show();
-	while(!visualizer.wasStopped()){
-		visualizer.spinOnce ();
 
-	}
+void Demo::setTargetCloud(const std::string &filename,
+        PointCloud<PointXYZRGB>::Ptr &target_cloud)
+{
+    io::loadPCDFile (filename, *target_cloud);
+    visualizer.setTargetCloud<PointXYZRGB>(target_cloud);
+}
+
+void Demo::transformClouds(PointCloud<PointXYZRGB>::Ptr source,
+        PointCloud<PointXYZRGB>::Ptr target)
+{
+    visualizer.setTransformedCloud<PointXYZRGB>(
+            transformer->transform(source, target));
+}
+
+void Demo::setOutputFile(const std::string& output) {}
+
+void Demo::enableKinect() {}
+
+void Demo::run()
+{
+    visualizer.show();
+    while(!visualizer.wasStopped()){
+        visualizer.spinOnce ();
+    }
 }
 
 int main (int argc, char **argv)
-    {
-    try{
-    // Parsing input parameters:
-    boost::program_options::options_description desc("Allowed options");
-    desc.add_options()
+{
+    try {
+        // Parsing input parameters:
+        boost::program_options::options_description desc("Allowed options");
+        desc.add_options()
         ("help", "produce help message")
-        ("i",boost::program_options::value<std::string>()->required(),"input file (required)")
+        ("s",boost::program_options::value<std::string>()->required(),"source cloud file (required)")
+        ("t",boost::program_options::value<std::string>()->required(),"target cloud file (required)")
         ("o",boost::program_options::value<std::string>(),"output file")
         ("k","enable Kinect")
     ;
@@ -46,21 +65,28 @@ int main (int argc, char **argv)
         return 1;
     }
 
-    boost::program_options::notify(vm);    
-    
+    boost::program_options::notify(vm);
+
     Demo demo;
     if(vm.count("k"))
         demo.enableKinect();
-    demo.setInputFile(vm["i"].as<std::string>());
+
+    PointCloud<PointXYZRGB>::Ptr source_cloud (new PointCloud<PointXYZRGB>);
+    PointCloud<PointXYZRGB>::Ptr target_cloud (new PointCloud<PointXYZRGB>);
+
+    demo.setSourceCloud(vm["s"].as<std::string>(), source_cloud);
+    demo.setTargetCloud(vm["t"].as<std::string>(), target_cloud);
+    demo.transformClouds(source_cloud, target_cloud);
+
     if(vm.count("o"))
         demo.setOutputFile(vm["o"].as<std::string>());
     demo.run();
     }
+
     /*catch(boost::program_options::error& e){
             std::cout<<"Exception in boost :"<<e.what();
     }*/
     catch(const std::exception& e){
         std::cout<<"Exception in programm:"<<e.what()<<std::endl;    
     }
-     
 }
--- demos/pct_cnn_demo/src/Demo.hpp
+++ demos/pct_cnn_demo/src/Demo.hpp
@@ -2,19 +2,40 @@
 #define DEMO_HEADER
 
 #include <string>
+
+#include <pcl/point_types.h>
+
 #include "DemoVisualizer.hpp"
 #include "PFHTransformStrategy.hpp"
-class Demo {
 
+using namespace std;
+using namespace pcl;
+
+class Demo
+{
 public:
+
     Demo();
+
     void enableKinect();
-    void setInputFile(const std::string& name);
-    void setOutputFile(const std::string& output);
+
+    void setSourceCloud(const std::string &name,
+            PointCloud<PointXYZRGB>::Ptr &source_cloud);
+
+    void setTargetCloud(const std::string &output,
+            PointCloud<PointXYZRGB>::Ptr &target_cloud);
+
+    void transformClouds(PointCloud<PointXYZRGB>::Ptr source,
+            PointCloud<PointXYZRGB>::Ptr target);
+
+    void setOutputFile(const std::string &output);
+
     void run();
+
 private:
-	DemoVisualizer	visualizer;
-	TransformStrategy *transformer;
+
+    DemoVisualizer visualizer;
+    TransformStrategy<PointXYZRGB> *transformer;
 
 };
 #endif
