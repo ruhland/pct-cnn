e93650593035cba5b41d44c46a205152874bc4fa
Andreas Ruhland <andi.ruhli@gmail.com>
2014-05-09 17:04:20 +0200

    configuration FEATUREFORMAT

--- demos/pct_cnn_demo/src/Configuration.cpp
+++ demos/pct_cnn_demo/src/Configuration.cpp
@@ -3,84 +3,91 @@
 #include <iostream>
 #include <stdexcept>
 
-Configuration::Configuration(){
+Configuration::Configuration() {
 	setDefaultConfigurations();
 }
 
-void Configuration::setDefaultConfigurations(){
-	filter=NOFILTER;
-	filterLeafSize=0.01f;
-	nearestNeighborsToSearch=5;
+void Configuration::setDefaultConfigurations() {
+	filter = NOFILTER;
+	filterLeafSize = 0.01f;
+	nearestNeighborsToSearch = 5;
 }
 
-
-void Configuration::loadFromFile(const std::string  &filename){
+void Configuration::loadFromFile(const std::string &filename) {
 	std::ifstream input(filename);
 	std::string line;
-	while(std::getline(input,line)){
-		size_t commentpos=line.find("#");
-		if(commentpos!= std::string::npos)
-			line=line.substr(0,commentpos);
-		line=trim(line);
-		size_t space= line.find(" ");
-		if(space== std::string::npos)
+	while (std::getline(input, line)) {
+		size_t commentpos = line.find("#");
+		if (commentpos != std::string::npos)
+			line = line.substr(0, commentpos);
+		line = trim(line);
+		size_t space = line.find(" ");
+		if (space == std::string::npos)
 			continue;
-		std::string property=trim(line.substr(0,space));
-		std::string value=trim(line.substr(space));
-		assignProperty(property,value);
+		std::string property = trim(line.substr(0, space));
+		std::string value = trim(line.substr(space));
+		assignProperty(property, value);
 	}
 }
 
-void Configuration::assignProperty(const std::string& property, const std::string& value){
-	std::cerr<<" ASSIGN Property "<<property<<"="<<value<<std::endl;
-	if(property.compare("FilterMethod")==0){
-		if(value.compare("NOFILTER")==0)
-			filter=NOFILTER;
-		else if(value.compare("VOXELGRIDFILTER")==0)
-			filter=VOXELGRIDFILTER;
+void Configuration::assignProperty(const std::string& property,
+		const std::string& value) {
+	std::cerr << " ASSIGN Property " << property << "=" << value << std::endl;
+	if (property.compare("FilterMethod") == 0) {
+		if (value.compare("NOFILTER") == 0)
+			filter = NOFILTER;
+		else if (value.compare("VOXELGRIDFILTER") == 0)
+			filter = VOXELGRIDFILTER;
 		else
-			std::cerr<<"WRONG Filter Method"<<value<<std::endl;
-	}
-	else if(property.compare("NearestNeighborsToSearch")==0){
-		try{
-			nearestNeighborsToSearch=std::stoi(value);
-		}
-		catch(const std::invalid_argument& e){
-			std::cerr<<"Wrong Argument "<<value<<std::endl;
+			std::cerr << "WRONG Filter Method" << value << std::endl;
+	} else if (property.compare("FeatureFormat") == 0) {
+		if (value.compare("PERFECTFEATURE") == 0)
+			featureFormat = PERFECTFEATURE;
+		else if (value.compare("PFHFEATURE") == 0)
+			featureFormat = PFHFEATURE;
+		else if (value.compare("XYZRGBFEATURE") == 0)
+			featureFormat = XYZRGBFEATURE;
+		else
+			std::cerr << "WRONG Feature Format" << value << std::endl;
+	} else if (property.compare("NearestNeighborsToSearch") == 0) {
+		try {
+			nearestNeighborsToSearch = std::stoi(value);
+		} catch (const std::invalid_argument& e) {
+			std::cerr << "Wrong Argument " << value << std::endl;
 		}
 
-	}
-	else if(property.compare("FILTERLEAFSIZE")==0){
-			try{
-				filterLeafSize=std::stof(value);
-			}
-			catch(const std::invalid_argument& e){
-				std::cerr<<"Wrong Argument "<<value<<std::endl;
-			}
-
+	} else if (property.compare("FILTERLEAFSIZE") == 0) {
+		try {
+			filterLeafSize = std::stof(value);
+		} catch (const std::invalid_argument& e) {
+			std::cerr << "Wrong Argument " << value << std::endl;
 		}
-	else{
-		std::cerr<<"Unknown Property "<<property<<std::endl;
+	} else {
+		std::cerr << "Unknown Property " << property << std::endl;
 	}
 
 }
 
-std::string Configuration::trim(const std::string& string){
-	size_t  start=string.find_first_not_of(" \t"); // First letter not " " or "\t"
-	if(start==std::string::npos)
+std::string Configuration::trim(const std::string& string) {
+	size_t start = string.find_first_not_of(" \t"); // First letter not " " or "\t"
+	if (start == std::string::npos)
 		return "";
-	size_t end= string.find_last_not_of(" \t");
-	return string.substr(start,end-start+1);
+	size_t end = string.find_last_not_of(" \t");
+	return string.substr(start, end - start + 1);
 }
 
-float Configuration::getFilterLeafSize(){
+float Configuration::getFilterLeafSize() {
 	return filterLeafSize;
 }
 
-FILTERMETHOD Configuration::getFilterMethod(){
+FEATUREFORMAT Configuration::getFeatureFormat(){
+	return featureFormat;
+}
+
+FILTERMETHOD Configuration::getFilterMethod() {
 	return filter;
 }
 
-int Configuration::getNearestNeighborsToSearch(){
+int Configuration::getNearestNeighborsToSearch() {
 	return nearestNeighborsToSearch;
 }
--- demos/pct_cnn_demo/src/Configuration.hpp
+++ demos/pct_cnn_demo/src/Configuration.hpp
@@ -2,6 +2,7 @@
 #define CONFIGURATION_HPP
 #include <string>
 enum FILTERMETHOD {NOFILTER,VOXELGRIDFILTER};
+enum FEATUREFORMAT {PERFECTFEATURE,XYZRGBFEATURE,PFHFEATURE};
 
 class Configuration{
 public:
@@ -10,11 +11,14 @@ public:
 	void loadFromFile(const std::string  &filename);
 	FILTERMETHOD getFilterMethod();
 	int getNearestNeighborsToSearch();
+	FEATUREFORMAT getFeatureFormat();
 	float getFilterLeafSize();
 private:
 	FILTERMETHOD filter;
+	FEATUREFORMAT featureFormat;
 	int nearestNeighborsToSearch;
 	float filterLeafSize;
+
 	void setDefaultConfigurations();
 	std::string trim(const std::string& string);
 	void assignProperty(const std::string& property, const std::string& value);
